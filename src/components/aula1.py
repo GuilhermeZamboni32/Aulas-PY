531f30875cf8bbc6a4622aef132dd88e52e395d1
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-03-05 10:17:00 -0300

    aula 1

---
+++ src/components/aula1.py
@@ -0,0 +1,34 @@
+
+Lista = [100, 35, 2, 28, 55]
+
+TamanhoLista = len(Lista)
+MaiorItem = max(Lista)
+MenorItem = min(Lista)
+
+print(lista)
+
+
+#///////////////////////////////////////////////////////////
+
+
+def fon (n):
+
+      if n > 1:
+        return n * foo (n - 1)
+    
+    return n
+    print(foo(4))
+
+
+#///////////////////////////////////////////////////////////
+
+def PegaUsuario
+
+Usuario = PegaUsuario ()
+
+if Usuario is 'Gui':
+    print('Oi, Adiministrador')
+
+    else print('Seja bem vindo {}'.format(Usuario))
+
+#///////////////////////////////////////////////////////////
